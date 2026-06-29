4aa1e996ad575b41b91619ca866bd77930f84374
nineeye <nineeye@nate.com>
2026-06-29 16:58:44 +0900

    Refactor Board class and tile creation logic
    Refactor Board class to improve structure and functionality.

--- game/Puzzle_Studio_Ultimate/games/sliding-puzzle/board.js
+++ game/Puzzle_Studio_Ultimate/games/sliding-puzzle/board.js
@@ -1,94 +1,92 @@
-export class Board {
-  constructor(size = 4) {
-    this.size = size;
+import Tile from "./tile.js";
 
-    this.tileSize = 100;
-    this.offsetX = 50;
-    this.offsetY = 50;
 
-    this.tiles = [];
-    this.empty = { x: size - 1, y: size - 1 };
+export default class Board {
 
-    this.moves = 0;
-    this.won = false;
 
-    this.init();
-    this.shuffle(120);
-  }
 
-  init() {
-    let id = 1;
+constructor(size=3){
 
-    for (let y = 0; y < this.size; y++) {
-      for (let x = 0; x < this.size; x++) {
-        if (x === this.size - 1 && y === this.size - 1) continue;
 
-        this.tiles.push({ x, y, id });
-        id++;
-      }
-    }
-  }
+this.size=size;
 
-  reset() {
-    this.tiles = [];
-    this.empty = { x: this.size - 1, y: this.size - 1 };
-    this.moves = 0;
-    this.won = false;
 
-    this.init();
-    this.shuffle(120);
-  }
+this.tiles=[];
 
-  shuffle(n) {
-    for (let i = 0; i < n; i++) {
-      const movable = this.tiles.filter(t => this.nearEmpty(t));
-      const pick = movable[Math.floor(Math.random() * movable.length)];
-      if (pick) this.swap(pick);
-    }
-  }
 
-  swap(t) {
-    const ex = this.empty.x;
-    const ey = this.empty.y;
+this.create();
 
-    this.empty.x = t.x;
-    this.empty.y = t.y;
 
-    t.x = ex;
-    t.y = ey;
 
-    this.moves++;
-  }
+}
 
-  getTileAt(x, y) {
-    return this.tiles.find(t => t.x === x && t.y === y);
-  }
 
-  getTileByPixel(px, py) {
-    const s = this.tileSize;
 
-    const x = Math.floor((px - this.offsetX) / s);
-    const y = Math.floor((py - this.offsetY) / s);
+create(){
 
-    return this.getTileAt(x, y);
-  }
 
-  nearEmpty(t) {
-    return Math.abs(t.x - this.empty.x) + Math.abs(t.y - this.empty.y) === 1;
-  }
+let number=1;
 
-  move(t) {
-    this.swap(t);
-  }
 
-  render(renderer) {
-    const s = this.tileSize;
 
-    for (const t of this.tiles) {
-      const x = this.offsetX + t.x * s;
-      const y = this.offsetY + t.y * s;
+for(let y=0;y<this.size;y++){
 
-      renderer.drawTile(t, x, y, s);
-    }
-  }
+
+for(let x=0;x<this.size;x++){
+
+
+if(number <
+this.size*this.size){
+
+
+this.tiles.push(
+
+new Tile(
+
+number,
+
+x*100,
+
+y*100,
+
+100
+
+)
+
+);
+
+
+}
+
+
+number++;
+
+
+}
+
+
+
+}
+
+
+
+}
+
+
+
+draw(ctx){
+
+
+this.tiles.forEach(
+
+tile=>tile.draw(ctx)
+
+);
+
+
+
+}
+
+
+
 }
