ea76a0fba63b2080bc01d02dacd277a4c1f84535
nineeye <nineeye@nate.com>
2026-06-29 16:54:41 +0900

    Enhance SceneManager with scene enter/exit handling
    Added enter and exit methods to handle scene transitions.

--- game/Puzzle_Studio_Ultimate/src/core/SceneManager.js
+++ game/Puzzle_Studio_Ultimate/src/core/SceneManager.js
@@ -1,31 +1,39 @@
 export default class SceneManager {
 
-    constructor() {
 
-        this.current = null;
+constructor(){
+
+    this.current=null;
+
+}
+
+
+
+change(scene){
+
+
+    if(this.current &&
+       this.current.exit){
+
+        this.current.exit();
+
+    }
+
+
+    this.current=scene;
+
+
+
+    if(scene.enter){
+
+        scene.enter();
 
     }
 
-    change(scene) {
 
-        this.current = scene;
 
-    }
+}
 
-    update(dt) {
 
-        if (this.current && this.current.update) {
-            this.current.update(dt);
-        }
-
-    }
-
-    render(ctx) {
-
-        if (this.current && this.current.render) {
-            this.current.render(ctx);
-        }
-
-    }
 
 }
