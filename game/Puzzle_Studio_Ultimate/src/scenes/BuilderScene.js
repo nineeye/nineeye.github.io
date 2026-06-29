f09a83209ab6f6da652e42586deceac01135a56a
nineeye <nineeye@nate.com>
2026-06-29 19:14:00 +0900

    Refactor BuilderScene to use class properties

--- game/Puzzle_Studio_Ultimate/src/scenes/BuilderScene.js
+++ game/Puzzle_Studio_Ultimate/src/scenes/BuilderScene.js
@@ -2,17 +2,31 @@ import Builder
 from "../core/Builder.js";
 
 
-
 export default class BuilderScene {
 
 
 
-constructor(){
+constructor(
+manager,
+input,
+plugins,
+audio
+){
 
 
-this.builder =
+this.manager=manager;
+
+
+this.builder=
 new Builder();
 
+
+
+}
+
+
+
+enter(){
 
 
 this.create();
@@ -27,22 +41,35 @@ create(){
 
 
 
-const box =
-document.createElement("div");
+this.box=document.createElement("div");
 
 
-
-box.style.position="absolute";
-
-
-box.style.top="50%";
+this.box.style.position="absolute";
 
 
-box.style.left="50%";
+this.box.style.top="50%";
 
 
-box.style.transform=
+this.box.style.left="50%";
+
+
+this.box.style.transform=
 "translate(-50%,-50%)";
+
+
+this.box.style.background="#111";
+
+
+this.box.style.color="white";
+
+
+this.box.style.padding="40px";
+
+
+this.box.style.textAlign="center";
+
+
+this.box.style.borderRadius="20px";
 
 
 
@@ -54,8 +81,23 @@ title.innerText=
 "Puzzle Builder";
 
 
+this.box.appendChild(title);
 
-box.appendChild(title);
+
+
+
+
+const text =
+document.createElement("p");
+
+
+text.innerText=
+"퍼즐 크기 설정";
+
+
+this.box.appendChild(text);
+
+
 
 
 
@@ -63,11 +105,13 @@ const size =
 document.createElement("input");
 
 
+size.type="number";
+
+
 size.value=3;
 
 
-size.type="number";
-
+size.style.fontSize="25px";
 
 
 size.onchange=()=>{
@@ -87,7 +131,9 @@ Number(size.value)
 
 
 
-box.appendChild(size);
+this.box.appendChild(size);
+
+
 
 
 
@@ -95,7 +141,18 @@ const save =
 document.createElement("button");
 
 
-save.innerText="SAVE";
+save.innerText=
+"SAVE";
+
+
+save.style.display="block";
+
+
+save.style.margin="20px auto";
+
+
+save.style.fontSize="22px";
+
 
 
 save.onclick=()=>{
@@ -104,10 +161,9 @@ save.onclick=()=>{
 this.builder.save();
 
 
-
 alert(
 
-"Project Saved"
+"PROJECT SAVED"
 
 );
 
@@ -117,19 +173,45 @@ alert(
 
 
 
-box.appendChild(save);
+this.box.appendChild(save);
 
 
 
-document.body.appendChild(box);
+
+
+const home =
+document.createElement("button");
+
+
+home.innerText=
+"HOME";
+
+
+
+home.style.fontSize="22px";
+
+
+
+home.onclick=()=>{
+
+
+location.reload();
+
+
+
+};
+
+
+
+this.box.appendChild(home);
+
+
+
+document.body.appendChild(this.box);
 
 
 
 }
-
-
-
-enter(){}
 
 
 
