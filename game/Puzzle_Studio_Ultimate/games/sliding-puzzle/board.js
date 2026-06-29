d4f23416001e7ea169507adc594da3e0ec426f33
nineeye <nineeye@nate.com>
2026-06-29 17:08:31 +0900

    Enhance Board class with shuffle and completion check
    Added shuffle and isComplete methods, updated tile creation logic.

--- game/Puzzle_Studio_Ultimate/games/sliding-puzzle/board.js
+++ game/Puzzle_Studio_Ultimate/games/sliding-puzzle/board.js
@@ -14,8 +14,10 @@ this.size=size;
 this.tiles=[];
 
 
-this.create();
+this.empty=size*size;
 
+
+this.create();
 
 
 }
@@ -25,8 +27,7 @@ this.create();
 create(){
 
 
-let number=1;
-
+let num=1;
 
 
 for(let y=0;y<this.size;y++){
@@ -35,15 +36,35 @@ for(let y=0;y<this.size;y++){
 for(let x=0;x<this.size;x++){
 
 
-if(number <
-this.size*this.size){
+
+if(num === this.size*this.size){
+
+this.tiles.push(
+
+new Tile(
+
+0,
+
+x*100,
+
+y*100,
+
+100
+
+)
+
+);
+
+
+
+}else{
 
 
 this.tiles.push(
 
 new Tile(
 
-number,
+num,
 
 x*100,
 
@@ -59,10 +80,160 @@ y*100,
 }
 
 
-number++;
+
+num++;
+
 
 
 }
+
+
+
+}
+
+
+
+}
+
+
+
+shuffle(){
+
+
+for(
+let i=this.tiles.length-1;
+i>0;
+i--
+){
+
+
+let j =
+Math.floor(
+Math.random()*(i+1)
+);
+
+
+
+[
+this.tiles[i],
+this.tiles[j]
+
+]
+=
+[
+this.tiles[j],
+this.tiles[i]
+
+];
+
+
+}
+
+
+
+this.updatePosition();
+
+
+}
+
+
+
+updatePosition(){
+
+
+this.tiles.forEach(
+(tile,index)=>{
+
+
+let x =
+index % this.size;
+
+
+let y =
+Math.floor(
+index/this.size
+);
+
+
+
+tile.x=x*100;
+
+tile.y=y*100;
+
+
+
+});
+
+
+
+}
+
+
+
+click(x,y){
+
+
+const index =
+this.tiles.findIndex(
+
+t=>t.contains(x,y)
+
+);
+
+
+
+if(index <0)
+return;
+
+
+
+const empty =
+this.tiles.findIndex(
+
+t=>t.value===0
+
+);
+
+
+
+const dx =
+Math.abs(
+(index%this.size)
+-
+(empty%this.size)
+);
+
+
+
+const dy =
+Math.abs(
+
+Math.floor(index/this.size)
+-
+Math.floor(empty/this.size)
+
+);
+
+
+
+if(dx+dy===1){
+
+
+
+[
+this.tiles[index],
+this.tiles[empty]
+
+]
+=
+[
+this.tiles[empty],
+this.tiles[index]
+
+];
+
+
+this.updatePosition();
 
 
 
@@ -80,6 +251,24 @@ draw(ctx){
 this.tiles.forEach(
 
 tile=>tile.draw(ctx)
+
+);
+
+
+
+}
+
+
+
+isComplete(){
+
+
+return this.tiles.every(
+
+(tile,index)=>
+
+tile.value===0 ||
+tile.value===index+1
 
 );
 
